7ebc26cefdf35b11790a30b9ed8e13a36d459d76
malakhovarthur <malakhov051@gmail.com>
2023-09-19 17:50:35 +0300

    add testimony section

---
+++ src/components/slider/Slider.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styled from "styled-components";
+import { FlexWrapper } from "../FlexWrapper";
+export const Slider = () => {
+  return (
+    <StyledSlider>
+      <FlexWrapper>
+        <Slide>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </Text>
+          <Name>@ivan ivanow</Name>
+        </Slide>
+      </FlexWrapper>
+
+      <PaginationStyled>
+        <span></span>
+        <span></span>
+        <span></span>
+      </PaginationStyled>
+    </StyledSlider>
+  );
+};
+
+const StyledSlider = styled.div`
+  border: 1px solid red;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Slide = styled.div`
+  text-align: center;
+`;
+
+const Text = styled.p``;
+
+const Name = styled.span``;
+
+const PaginationStyled = styled.div`
+  span {
+    display: inline-block;
+    width: 20px;
+    height: 20px;
+    margin: 5px;
+    background-color: deeppink;
+  }
+`;
